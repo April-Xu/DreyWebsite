efa3259958a8428f791b31f96612dd2a88bed389
xmj <115010078@link.cuhk.edu.cn>
2020-08-01 15:27:40 -0400

    upload template

---
+++ Bolo/Bolo HTML/js/countdown-custom.js
@@ -0,0 +1,7 @@
+(function($) {	
+	'use strict';
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2020, 12-1, 1, 8)}); // year, month, date, hour
+        });
+})(jQuery);
+
